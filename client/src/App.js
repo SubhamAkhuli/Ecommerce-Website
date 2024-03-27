6b64b8ea948626d9dc75ae20f479aabea3048494
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-03-27 13:24:25 +0530

    Add React Router and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,62 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
+import Policy from "./pages/Policy";
+import Contact from "./pages/Contact";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
+  // new react router process create a router
+  const router = createBrowserRouter([
+    {
+      // path is the url path
+      path: "/",
+      // element is the component to be rendered
+      element: (
+        <>
+          <Homepage />
+        </>
+      ),
+    },
+    {
+      path: "/about",
+      element: (
+        <>
+          <About />
+        </>
+      ),
+    },
+    {
+      path: "/policy",
+      element: (
+        <>
+          <Policy />
+        </>
+      ),
+    },
+    {
+      path: "/contact",
+      element: (
+        <>
+          <Contact />
+        </>
+      ),
+    },
+    {
+      path: "*",
+      element: (
+        <>
+          <PageNotFound />
+        </>
+      ),
+    },
+  ]);
   return (
-<>
-  <h1>Ecommerce App</h1>
-</>
+    <>
+      {/* to call the pages */}
+      <RouterProvider router={router} />
+    </>
   );
 }
 
