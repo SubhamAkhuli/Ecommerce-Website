ec06da8b3f83afd3f867a5b8255f1988727fb87d
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-03-30 00:19:35 +0530

    Update PageNotFound component with new design

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import Header from '../components/Layout/Header'
 import Footer from '../components/Layout/Footer'
+import { NavLink } from 'react-router-dom'
 
 function PageNotFound() {
   return (
     <div>
       <Header />
-      <div className="container" style={{ minHeight: "70.6vh" }}>
-        <h1 className="text-center" >404 Page Not Found</h1> 
+      <div className="container" style={{ minHeight: "69.8vh" }}>
+      {/* <div className='d-flex justify-content-center mt-5'
+      >
+        <h1 className='mt-5' style={{color:"red"}} >404 Page Not Found!</h1> 
+        </div> */}
+
+        <div className="pnf">
+        <h1 className="pnf-title">404</h1>
+        <h2 className="pnf-heading">Oops ! Page Not Found</h2>
+        <NavLink to="/" className="btn btn-primary">
+          Go Back
+        </NavLink>
+      </div>
         </div>
         <Footer/>
     </div>
