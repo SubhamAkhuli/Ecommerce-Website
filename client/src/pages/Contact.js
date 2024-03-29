043abab547e6ce2558ad35c4c8d5b91c6a74e78e
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-03-30 00:19:19 +0530

    Refactor Contact page layout

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -8,7 +8,7 @@ function Contact() {
     <div>
       <Header />
         <div className="text-center" style={{ minHeight: "67.5vh" }}>
-          <h1 className="my-3">Contact Us</h1>
+          {/* <h1 className="my-3">Contact Us</h1>
         <p className="my-2">
             <stonge>Any query and info about prodduct feel free to call anytime we 24X7
             vaialible</stonge>
@@ -21,7 +21,33 @@ function Contact() {
           </p>
           <p className="mt-3">
             <BiSupport /> : 1800-0000-0000 (toll free)
+          </p> */}
+
+          <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <h1 className=" text-center">Contact Us</h1>
+          <p className="text-justify mt-2">
+            any query and info about prodduct feel free to call anytime we 24X7
+            vaialible
           </p>
+          <p className="mt-3">
+            <BiMailSend /> : www.help@ecommerceapp.com
+          </p>
+          <p className="mt-3">
+            <BiPhoneCall /> : 012-3456789
+          </p>
+          <p className="mt-3">
+            <BiSupport /> : 1800-0000-0000 (toll free)
+          </p>
+        </div>
+      </div>
         </div>
         <Footer />
     </div>
