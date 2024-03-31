cf8426ca689d50cfafbda36b2d233b9347b1b567
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-03-31 12:20:29 +0530

    Refactor server.js file

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import connectDB from './config/db.js';
-import authUserRoute from './routes/user/authUserRoute.js';
-import authSellerRoute from './routes/seller/authSellerRoute.js';
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authUserRoute from "./routes/user/authUserRoute.js";
+import authSellerRoute from "./routes/seller/authSellerRoute.js";
+import cors from "cors";
 
 // load env variables
 dotenv.config();
-
 
 // connect to database
 connectDB();
@@ -16,20 +16,19 @@ connectDB();
 const app = express();
 
 // middlewares
+app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
-
+app.use(morgan("dev"));
 
 // routes for user
-app.use('/api/v1/userauth', authUserRoute);
-
+app.use("/api/v1/userauth", authUserRoute);
 
 // routes for seller
-app.use('/api/v1/sellerauth', authSellerRoute);
+app.use("/api/v1/sellerauth", authSellerRoute);
 
 // rest api
-app.get('/', (req, res) => {
-    res.send("<h1> Wellcome to My Ecommerce Website</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1> Wellcome to My Ecommerce Website</h1>");
 });
 
 // set port, listen for requests
@@ -37,5 +36,5 @@ const PORT = process.env.PORT || 8080;
 
 // run listen
 app.listen(PORT, () => {
-    console.log(`Server is running in ${process.env.DEV_MODE} on port ${PORT}`);
+  console.log(`Server is running in ${process.env.DEV_MODE} on port ${PORT}`);
 });
