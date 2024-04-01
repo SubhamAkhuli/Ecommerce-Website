2bf06df6bb76e464711077e95d3d69a3b157c55a
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-02 00:34:26 +0530

    Add user information to homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../components/Layout/Header";
 import Footer from "../components/Layout/Footer";
+import { useAuth } from "../context/auth";
 function Homepage() {
+  const { user,setUser } = useAuth();
   return (
     <div>
       <Header />
       <div className="container" style={{ minHeight: "68.6vh" }}>
         <h1 className="text-center mt-2">Welcome to the Homepage</h1>
+        <pre>{JSON.stringify(user, null, 2)}</pre>
       </div>
       <Footer />
     </div>
