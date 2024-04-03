3bb0d37180eff96fb7745f46afefc8ac56768890
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-03 21:32:28 +0530

    Add userForgotPasswordController to authUserRoute.js

--- routes/user/authUserRoute.js
+++ routes/user/authUserRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {userRegisterController,userLoginController,testcontroller} from "../../controllers/user/authUserController.js";
+import {userRegisterController,userLoginController,testcontroller,userForgotPasswordController} from "../../controllers/user/authUserController.js";
 import {authUserMiddleware} from "../../middlewares/User/authUserMiddleware.js";
 // router object
 const router = express.Router();
@@ -15,6 +15,9 @@ router.post("/userlogin", userLoginController);
 
 // testing route
 router.get("/test",authUserMiddleware, testcontroller);
+
+// forgot password
+router.post("/userforgotpassword", userForgotPasswordController);
 
 // Protected route
 router.get("/user-auth",authUserMiddleware, (req, res) => {
