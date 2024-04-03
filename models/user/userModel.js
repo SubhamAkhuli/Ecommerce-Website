36ea3bb6ca4a6f8bad61d5f8183f2ab1cf1e5ad5
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-03 21:32:34 +0530

    Add answer field to user model

--- models/user/userModel.js
+++ models/user/userModel.js
@@ -23,6 +23,10 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
     },
+    answer:{
+        type: String,
+        required: true,
+    }
     },
     {timestamps: true},);
 
