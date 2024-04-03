bbdb611bf7ac2080958ed21853f012358984db15
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-03 21:34:07 +0530

    Refactor sellerModel.js schema

--- models/seller/sellerModel.js
+++ models/seller/sellerModel.js
@@ -1,39 +1,45 @@
 import mongoose from "mongoose";
 
-const sellorSchema = mongoose.Schema({
+const sellorSchema = mongoose.Schema(
+  {
     shop_name: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     category: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     name: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    phone:{
-        type: String,
-        required: true,
+    phone: {
+      type: String,
+      required: true,
     },
-    address:{
-        type: String,
-        required: true,
-    }
+    address: {
+      type: String,
+      required: true,
     },
-    {timestamps: true},);
+    answer: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 const Seller = mongoose.model("sellers", sellorSchema);
 
