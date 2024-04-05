67f6b82e1203aa37f50729a783b9372be0c8b639
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-05 12:29:48 +0530

    Refactor UserPrivateRoute component to use destructured useState and add type check in SellerPrivate.js

--- client/src/components/Routes/seller/SellerPrivate.js
+++ client/src/components/Routes/seller/SellerPrivate.js
@@ -5,25 +5,25 @@ import { Outlet } from "react-router-dom";
 import Spinner from "../../Layout/Spinner";
 export default function UserPrivateRoute() {
   const [ok, setOk] = useState(false);
-  const { user } = useAuth();
+  const [user]  = useAuth();
+  const type =  user?.user?.type;
   useEffect(() => {
+    if (type === "seller"&& user?.token)
+    {
     const authcheck = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:8080/api/v1/selllerauth/user-auth "
-        );
-        if (response.status === 200) {
-          setOk(true);
-        } else {
-          setOk(false);
-        }
-      } catch (error) {
-        console.error("Error during authentication check:", error);
-        console.log("User is not authenticated");
+      const res = await axios.get("http://localhost:8080/api/v1/sellerauth/seller-auth");
+      if (res.data.ok) {
+        setOk(true);
+      } else {
         setOk(false);
       }
     };
     if (user?.token) authcheck();
-  }, [user?.token]);
+  }
+  else{
+    setOk(false);
+  }
+
+}, [user?.token , type]);
   return ok ? <Outlet /> : <Spinner  path=""/>;
 }
