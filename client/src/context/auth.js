54ebce6f21b5281683111e388fb36e588e77bb3c
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-05 12:28:17 +0530

    Refactor authSellerMiddleware to authMiddleware in authSellerRoute.js Refactor authUserMiddleware to authMiddleware in authUserRoute.js Remove authSellerMiddleware and authUserMiddleware Add admin authentication route and related files Add onChange event and value attribute to select element in ForgotPassword.js Remove console.log statements in Login.js Add admin authentication route Add authMiddleware for protected routes Add admin login controller Update login page with new links for forgot password and seller registration Update login route in userRegister.js

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,7 +1,8 @@
 import {useState,useEffect, createContext, useContext} from 'react';
 import axios from 'axios';
-const AuthContext = createContext();
 
+
+const AuthContext = createContext();
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState({
         user: null,
@@ -13,12 +14,15 @@ const AuthProvider = ({children}) => {
 
 
     useEffect(() => {
-        const userData = JSON.parse(localStorage.getItem("user"));
-        if (userData) {
+        const data = localStorage.getItem("user");
+        // const userData = JSON.parse(localStorage.getItem("user"));
+        if (data) {
+            const parseData = JSON.parse(data);
             setUser(
                 {
-                    user: userData.user,
-                    token: userData.token,
+                    ...user,
+                    user: parseData.user,
+                    token: parseData.token,
                 }
             );
         }
@@ -26,7 +30,7 @@ const AuthProvider = ({children}) => {
     }, []);
 
     return (
-        <AuthContext.Provider value={{user, setUser}}>
+        <AuthContext.Provider value={[user, setUser]}>
             {children}
         </AuthContext.Provider>
     );
