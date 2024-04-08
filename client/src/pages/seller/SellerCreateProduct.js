9109be8d2f356bd3aa17eb03e775e5713ddff022
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-08 15:53:35 +0530

    Add SellerCreateProduct component for seller to create a new product

---
+++ client/src/pages/seller/SellerCreateProduct.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from "../../components/Layout/Header";
+import Footer from "../../components/Layout/Footer";
+import SellerMenu from "../../pages/seller/SellerMenu";
+const SellerCreateProduct = () => {
+    return (
+        <>
+        <Header />
+        <div className="container m-3" style={{ minHeight: "56vh" }}>
+            <div className="row">
+            <div className="col-md-3">
+                <SellerMenu />
+            </div>
+            <div className="col-md-9">
+                <div className="card">
+                <div className="card-header text-center">
+                <h3>Create Product</h3>
+                </div>
+                <div className="card-body">
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <Footer />
+        </>
+    );
+}
+
+export default SellerCreateProduct;
