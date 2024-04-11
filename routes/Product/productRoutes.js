1f2f8d766ef31ed77f71c9170a2e52bc92e02383
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-11 21:07:22 +0530

    Add product routes for managing products in the application

---
+++ routes/Product/productRoutes.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { createProductController,getProductController,getSingleProductController,productPhotoController,updateProductController,deleteProductController } from '../../controllers/Product/productController.js';
+import { authMiddleware } from '../../middlewares/authMiddleware.js';
+import ExpressFormidable from 'express-formidable';
+
+const router = express.Router();
+
+// Create a product
+router.post('/create-product',authMiddleware,ExpressFormidable(), createProductController);
+
+// get all products
+router.get('/get-product/:id', getProductController);
+
+// get single product
+router.get('/getone-product/:pid', getSingleProductController);
+
+// get photo
+router.get('/product-photo/:pid', productPhotoController);
+
+// update product
+router.put('/update-product/:pid',authMiddleware,ExpressFormidable(), updateProductController);
+
+// delete product
+router.delete('/delete-product/:pid',authMiddleware, deleteProductController);
+
+export default router;
