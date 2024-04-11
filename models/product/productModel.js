9630ba4466ff26b90450ea0f568ea44d439a63cd
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-11 21:07:15 +0530

    Add product model for managing products in the application

---
+++ models/product/productModel.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+
+const productSchema = new mongoose.Schema({
+    seller: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'sellers',
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        data: Buffer,
+        contentType: String,
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    shipping: {
+        type: String,
+        default:"In Stock",
+        required: true
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+const Product = mongoose.model('Products', productSchema);
+
+export default Product;
