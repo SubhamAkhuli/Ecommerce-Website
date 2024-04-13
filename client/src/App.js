96ae5f01359880b3af222f1a97167cb310e48099
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-13 16:30:34 +0530

    Update routes for user and seller registration in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,18 @@ import About from "./pages/About";
 import Policy from "./pages/Policy";
 import Contact from "./pages/Contact";
 import PageNotFound from "./pages/PageNotFound";
-import UserRegister from "./pages/user/userRegister";
-import SellerRegister from "./pages/seller/sellerRegister";
 import Login from "./pages/Auth/Login.js";
 // user links
+import UserRegister from "./pages/user/userRegister";
 import UserDashboard from "./pages/user/UserDashboard";
 import UserPrivateRoute from "./components/Routes/user/UserPrivate.js";
 import UserOrders from "./pages/user/UserOrders.js";
 import UserWishlist from "./pages/user/UserWishlist.js";
-import UserChangePassword from "./pages/user/UserChangePassword.js"; 
+import UserChangePassword from "./pages/user/UserChangePassword.js";
 
 // seller links
+import SellerRegister from "./pages/seller/sellerRegister";
+import Sellerverification from "./pages/seller/Sellerverification.js";
 import SellerDashboard from "./pages/seller/SellerDashboard.js";
 import SellerPrivateRoute from "./components/Routes/seller/SellerPrivate.js";
 import SellerCreateProduct from "./pages/seller/SellerCreateProduct.js";
@@ -26,6 +27,7 @@ import SellerOrders from "./pages/seller/SellerOrders.js";
 import SellerChangePassword from "./pages/seller/SellerChangePassword.js";
 
 // admin links
+import AdminHome from "./pages/admin/AdminHome.js";
 import AdminDashboard from "./pages/admin/AdminDashboard.js";
 import AdminPrivateRoute from "./components/Routes/admin/AdminPrivate.js";
 import AdminAllProducts from "./pages/admin/AdminAllProducts.js";
@@ -47,39 +49,57 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/policy" element={<Policy />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/userregister" element={<UserRegister />} />
-            <Route path="/sellerregister" element={<SellerRegister />} />
             <Route path="/login" element={<Login />} />
             <Route path="/forgotpassword" element={<ForgotPassword />} />
             {/* Nested routes */}
             {/* User routes */}
+            <Route path="/userregister" element={<UserRegister />} />
             <Route path="/dashboard" element={<UserPrivateRoute />}>
               <Route path="user" element={<UserDashboard />} />
               <Route path="user/orders" element={<UserOrders />} />
               <Route path="user/wishlist" element={<UserWishlist />} />
-              <Route path="user/changepassword" element={<UserChangePassword />} />
+              <Route
+                path="user/changepassword"
+                element={<UserChangePassword />}
+              />
             </Route>
 
             {/* Seller routes */}
+            <Route
+              path="/sellerverification"
+              element={<Sellerverification />}
+            />
+            <Route path="/sellerregister" element={<SellerRegister />} />
             <Route path="/dashboard" element={<SellerPrivateRoute />}>
               <Route path="seller" element={<SellerDashboard />} />
-              <Route path="seller/create-product" element={<SellerCreateProduct />} />
+              <Route
+                path="seller/create-product"
+                element={<SellerCreateProduct />}
+              />
               <Route path="seller/products" element={<SellerProducts />} />
-              <Route path="seller/update-product/:pid" element={<SellerUpdateProduct />} />
+              <Route
+                path="seller/update-product/:pid"
+                element={<SellerUpdateProduct />}
+              />
               <Route path="seller/orders" element={<SellerOrders />} />
-              <Route path="seller/changepassword" element={<SellerChangePassword />} />
+              <Route
+                path="seller/changepassword"
+                element={<SellerChangePassword />}
+              />
             </Route>
 
             {/* Admin routes */}
-            <Route path="/dashboard" element={<AdminPrivateRoute />} >
-              <Route path="admin" element={<AdminDashboard/>} />
+            <Route path="/dashboard" element={<AdminPrivateRoute />}>
+              <Route path="admin" element={<AdminDashboard />} />
+              <Route path="adminhome" element={<AdminHome />} />
               <Route path="admin/all-product" element={<AdminAllProducts />} />
               <Route path="admin/users-list" element={<AdminUserLists />} />
               <Route path="admin/sellers-list" element={<AdminSellerLists />} />
-              <Route path="admin/seller-specific-product/:pid" element={<SellerSpceficProduct />} />
+              <Route
+                path="admin/seller-specific-product/:pid"
+                element={<SellerSpceficProduct />}
+              />
             </Route>
-
-
 
             {/* 404 Page Not Found */}
             <Route path="*" element={<PageNotFound />} />
