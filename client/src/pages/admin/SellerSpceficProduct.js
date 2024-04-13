6f48dec69253681d784dab854b34c5207c801ed7
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-14 00:07:36 +0530

    Update SellerSpceficProduct.js to improve product details display

--- client/src/pages/admin/SellerSpceficProduct.js
+++ client/src/pages/admin/SellerSpceficProduct.js
@@ -83,43 +83,19 @@ const SellerSpceficProduct = () => {
                         className="card-img-top"
                         style={{
                           height: "200px",
-                          width: "150px",
-                          margin: "auto",
                           objectFit: "contain",
-                          marginTop: "5px",
+                          marginTop: "10px",
                         }}
                         alt={product.name}
                       />
                       <div className="card-body">
-                        <h5 className="card-title">
-                          <b>{product.name}</b>
-                        </h5>
-                        <p
-                          className="card-text"
-                          style={{ marginBottom: "2px" }}
-                        >
-                          {product.description}
-                        </p>
-                        <p
-                          className="card-text"
-                          style={{ marginBottom: "2px" }}
-                        >
-                          <b>Price: ₹</b>
-                          {product.price}
-                        </p>
-                        <p
-                          className="card-text"
-                          style={{ marginBottom: "2px" }}
-                        >
-                          <b>Brand:</b> {product.brand}
-                        </p>
-                        <p
-                          className="card-text"
-                          style={{ marginBottom: "2px" }}
-                        >
-                          <b>Quantity:</b> {product.quantity}
-                        </p>
-                      </div>
+                          <h5 className="card-title">{product.name}</h5>
+                          <p className="card-text"><b>Brand: </b>{product.brand}</p> 
+                          <p className="card-text"><b>Price: </b>₹{product.price}</p>
+                          <p className="card-text"><b>Quantity: </b>{product.quantity}</p>
+                          <p className="card-text"><b>Category: </b>{product.category}</p>
+                          <p className="card-text"><b>Description: </b>{product.description}</p>
+                        </div>
                     </div>
                   ))}
                 </div>
