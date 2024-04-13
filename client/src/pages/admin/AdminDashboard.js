2ef4c17e26d931035b1c1a31dfed7669bf759978
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-13 16:31:47 +0530

    Refactor AdminDashboard component to display user details and add AdminMenu

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,34 +1,91 @@
 import React from "react";
-import Header from '../../components/Layout/Header'
-import Footer from '../../components/Layout/Footer'
+import Header from "../../components/Layout/Header";
+import Footer from "../../components/Layout/Footer";
+import AdminMenu from "./AdminMenu";
 import { useAuth } from "../../context/auth";
 
-const  AdminDashboard = () => {
-  const [user]= useAuth();
+const AdminDashboard = () => {
+  const [user] = useAuth();
   const userName = user?.user?.name || "N/A";
   const userEmail = user?.user?.email || "N/A";
   const userAddress = user?.user?.address || "N/A";
+  const userPhone = user?.user?.phone || "N/A";
   return (
     <>
-    <Header/>
-      <div className="container-flui m-3 p-3 dashboard">
+      <Header />
+      <div className="container m-3" style={{ minHeight: "65.2vh" }}>
         <div className="row">
           <div className="col-md-3">
-            {/* <UserMenu /> */}
+            <AdminMenu />
           </div>
           <div className="col-md-9">
-            <div className="card w-75 p-3">
-              <h3>{userName}</h3>
-              <h3>{userEmail}</h3>
-              <h3>{userAddress}</h3>
+            <div className="card">
+              <div className="card-header text-center">
+                <h3>Welcome {userName}</h3>
+              </div>
+              <div className="card-body">
+                <form>
+                  <div className="form-floating mb-3">
+                    <input
+                      disabled
+                      type="text"
+                      className="form-control"
+                      id="floatingInput"
+                      placeholder="Enter your Name"
+                      value={userName}
+                      name="name"
+                      required
+                    />
+                    <label htmlFor="floatingInput">Name</label>
+                  </div>
+                  <div className="form-floating mb-3">
+                    <input
+                      disabled
+                      type="email"
+                      className="form-control"
+                      id="floatingInput"
+                      placeholder="Enter your Shop Type"
+                      value={userEmail}
+                      name="email"
+                      required
+                    />
+                    <label htmlFor="floatingInput">Email</label>
+                  </div>
+                  <div className="form-floating mb-3">
+                    <input
+                      disabled
+                      type="text"
+                      className="form-control"
+                      id="floatingInput"
+                      placeholder="Enter your Phone"
+                      value={userPhone}
+                      name="phone"
+                      required
+                    />
+                    <label htmlFor="floatingInput">Phone</label>
+                  </div>
+                  <div className="form-floating mb-3">
+                    <input
+                      disabled
+                      type="text"
+                      className="form-control"
+                      id="floatingInput"
+                      placeholder="Enter your Address"
+                      value={userAddress}
+                      name="address"
+                      required
+                    />
+                    <label htmlFor="floatingInput">Address</label>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    <Footer/>
+      <Footer />
     </>
   );
 };
 
-export default  AdminDashboard;
-
+export default AdminDashboard;
