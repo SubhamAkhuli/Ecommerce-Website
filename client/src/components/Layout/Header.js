abcc8254977aa0c682cda38a9981e7028ebd587e
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-16 21:07:32 +0530

    Refactor routing paths for improved consistency and navigation in App.js, Login.js, and AdminMenu.js

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,23 +1,25 @@
 import React from "react";
-import logo from "../Layout/logo.jpg";
 import { NavLink } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import toast from "react-hot-toast";
+import logo from "../Layout/logo.jpg";
 import { useAuth } from "../../context/auth";
+import { toast } from "react-hot-toast";
 
-function Header(props) {
+const Header = (props) => {
   const { username } = props;
   const [user, setUser] = useAuth();
-  const HandelLogout = () => {
+
+  const handleLogout = () => {
     setUser({
       user: null,
       token: "",
     });
     localStorage.removeItem("user");
     setTimeout(() => {
-      toast.success("Logout Successfully");
+      toast.success("Logged out successfully");
     }, 100);
   };
+
   return (
     <>
       <Toaster />
@@ -27,38 +29,164 @@ function Header(props) {
             style={{ width: "40px", height: "35px", borderRadius: "50%" }}
             src={logo}
             alt="logo"
-          ></img>
-          {!user.user ? (
-            <>
-              <NavLink className="navbar-brand mx-2" to="/">
-                Ecommerce App
-              </NavLink>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
+          />
+          <NavLink className="navbar-brand mx-2" to="/">
+            Ecommerce App
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            {user?.user ? (
+              <>
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <NavLink
-                      className="nav-link active"
-                      aria-current="page"
-                      to="/"
-                    >
+                  <li className="nav-item ms-3">
+                    <NavLink className="nav-link" to={`/dashboard/${user.user.type}`}>
                       Home
                     </NavLink>
                   </li>
+                  {user.user.type === "admin" && (
+                    <>
+                    <li className="nav-item ms-3">
+                      <NavLink
+                        className="nav-link"
+                        to="/dashboard/admin/users-list"
+                      >
+                        Users
+                      </NavLink>
+                    </li>
+                    <li className="nav-item ms-3">
+                      <NavLink
+                        className="nav-link"
+                        to="/dashboard/admin/sellers-list"
+                      >
+                        Sellers
+                      </NavLink>
+                    </li>
+                    </>
+            )}
+                  {user.user.type === "seller" && (
+                    <li className="nav-item ms-3">
+                      <NavLink className="nav-link"  to="/dashboard/seller/create-product"
+                    >
+                    Add Product
+                      </NavLink>
+                    </li>
+                  )}
                 </ul>
+                {user.user.type === "user" && (
+                  <form className="d-flex" role="search">
+                    <input
+                      className="form-control me-2"
+                      type="search"
+                      placeholder="Search"
+                      aria-label="Search"
+                    />
+                    <button
+                      className="btn btn-outline-success"
+                      type="submit"
+                    >
+                      Search
+                    </button>
+                  </form>
+                )}
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <li className="nav-item dropdown mx-3">
+                    <NavLink
+                      className="nav-link dropdown-toggle"
+                      to=""
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      {username || user.user.name}
+                    </NavLink>
+                    <ul className="dropdown-menu dropdown-menu-dark">
+                      <li>
+                        <NavLink
+                          className="dropdown-item"
+                          to={`/dashboard/${user.user.type}/profile`}
+                        >
+                          <i className="bi bi-person-circle m-1"></i> My Profile
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          className="dropdown-item"
+                          to="/login"
+                          onClick={handleLogout}
+                        >
+                          <i className="fa-solid fa-power-off m-1"></i> Logout
+                        </NavLink>
+                      </li>
+                    </ul>
+                  </li>
+                  {user.user.type === "seller" && (
+                    <>
+                      <li className="nav-item me-3">
+                        <NavLink
+                          className="nav-link"
+                          to="/dashboard/seller/orders"
+                        >
+                          Orders
+                        </NavLink>
+                      </li>
+                      <li className="nav-item me-3">
+                        <NavLink
+                          className="nav-link"
+                          to="/dashboard/seller/products"
+                        >
+                          Products
+                        </NavLink>
+                      </li>
+                    </>
+                  )}
+                  {user.user.type === "user" && (
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/cart">
+                        Cart(0)
+                      </NavLink>
+                    </li>
+                  )}
+                  {user.user.type === "admin" && (
+                    <>
+                       <li className="nav-item me-3">
+                        <NavLink
+                          className="nav-link"
+                          to="/dashboard/admin/all-product"
+                        >
+                          Products
+                        </NavLink>
+                      </li>
+                      <li className="nav-item me-3">
+                        <NavLink
+                          className="nav-link"
+                          to="/dashboard/admin/orders"
+                        >
+                          Orders
+                        </NavLink>
+                      </li>
+                    </>
+                  )}
+                </ul>
+              </>
+            ) : (
+              <>
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/">
+                      Home
+                    </NavLink>
+                  </li>
+                  </ul>
                 <form className="d-flex" role="search">
                   <input
                     className="form-control me-2"
@@ -66,7 +194,10 @@ function Header(props) {
                     placeholder="Search"
                     aria-label="Search"
                   />
-                  <button className="btn btn-outline-success" type="submit">
+                  <button
+                    className="btn btn-outline-success"
+                    type="submit"
+                  >
                     Search
                   </button>
                 </form>
@@ -87,103 +218,18 @@ function Header(props) {
                     </NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink className="nav-link" to="/cart">
-                      Cart(0)
-                    </NavLink>
-                  </li>
+                      <NavLink className="nav-link" to="/cart">
+                        Cart(0)
+                      </NavLink>
+                    </li>
                 </ul>
-              </div>
-            </>
-          ) : (
-            <>
-              <NavLink
-                className="navbar-brand mx-2"
-                to={`/dashboard/${user?.user?.type}`}
-              >
-                Ecommerce App
-              </NavLink>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <NavLink
-                      className="nav-link active"
-                      aria-current="page"
-                      to={`/dashboard/${user?.user?.type}`}
-                    >
-                      Home
-                    </NavLink>
-                  </li>
-                </ul>
-                <form className="d-flex" role="search">
-                  <input
-                    className="form-control me-2"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
-                  />
-                  <button className="btn btn-outline-success" type="submit">
-                    Search
-                  </button>
-                </form>
-                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                  <li className="nav-item dropdown  mx-3">
-                    <NavLink
-                      className="nav-link dropdown-toggle"
-                      to=""
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      {/* {user.user.name} */}
-                      {username ? username : user.user.name}
-                    </NavLink>
-                    <ul className="dropdown-menu dropdown-menu-dark  ">
-                      <li>
-                        <NavLink
-                          className="dropdown-item"
-                          to={`/dashboard/${user?.user?.type}/profile`}
-                        >
-                          <i className="bi bi-person-circle m-1"></i> My Profile
-                        </NavLink>
-                      </li>
-                      <li>
-                        <NavLink
-                          onClick={HandelLogout}
-                          className="dropdown-item"
-                          to="/login"
-                        >
-                          <i className="fa-solid fa-power-off m-1"></i> Logout
-                        </NavLink>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/cart">
-                      Cart(0)
-                    </NavLink>
-                  </li>
-                </ul>
-              </div>
-            </>
-          )}
+              </>
+            )}
+          </div>
         </div>
       </nav>
     </>
   );
-}
+};
 
 export default Header;
