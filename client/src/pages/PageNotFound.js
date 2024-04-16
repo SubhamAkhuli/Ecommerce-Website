daf812973f9a8e4541c84fd27dc93154c60aa7d2
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-16 23:40:33 +0530

    Update PageNotFound.js to display a custom image for page not found

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,33 +1,20 @@
 import React from "react";
 import Header from "../components/Layout/Header";
 import Footer from "../components/Layout/Footer";
-import { useNavigate } from "react-router-dom";
+import pagenotfoundImg from "./pageNotFound.jpg";
 
 function PageNotFound() {
-  const navigate = useNavigate();
   return (
     <div>
       <Header />
       <div className="container" style={{ minHeight: "69.8vh" }}>
         <div className="pnf">
-          <h1 className="pnf-title">404</h1>
-          <h2 className="pnf-heading">Oops ! Page Not Found</h2>
-          <p className="pnf-para">
-            The page you are looking for might have been removed had its name
-            changed or is temporarily unavailable.
-          </p>
-          <button
-            className="btn btn-primary"
-            onClick={() => {
-              navigate(-1);
-            }}
-          >
-            <i
-              className="bi bi-backspace me-2"
-              style={{ cursor: "pointer", marginBottom: "2px" }}
-            ></i>
-            Back
-          </button>
+          <img
+            src={pagenotfoundImg}
+            alt="Page Not Found"
+            className="pnf-img"
+            style={{ width: "500px", height: "auto" }}
+          />
         </div>
       </div>
       <Footer />
