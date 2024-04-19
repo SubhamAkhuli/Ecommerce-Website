c96a2cfd07bea4942a05761bc44914ff31337bb8
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-19 20:59:57 +0530

    Add Cart.js file to implement shopping cart functionality

---
+++ client/src/context/Cart.js
@@ -0,0 +1,24 @@
+import {useState,createContext, useContext,useEffect} from 'react';
+
+
+const CartContext = createContext();
+const CartProvider = ({children}) => {
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        const cart = localStorage.getItem("cart");
+        if (cart) {
+            setCart(JSON.parse(cart));
+        }
+    }, []);
+    return (
+        <CartContext.Provider value={[cart,setCart]}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+ const useCart = () => useContext(CartContext);
+
+    export {useCart, CartProvider};
+
