8ac25a121d24364b5ed507c211e80d0de64e7fff
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-21 12:52:28 +0530

    Add Go Back button to PageNotFound.js

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "../components/Layout/Header";
 import Footer from "../components/Layout/Footer";
 import pagenotfoundImg from "./pageNotFound.jpg";
+import { useNavigate } from "react-router-dom";
 
 function PageNotFound() {
+  const navigate = useNavigate();
   return (
     <div>
       <Header />
       <div className="container" style={{ minHeight: "69.8vh" }}>
+      <button onClick={() => navigate(-1)} style={{boxShadow:"0 0 10px #ccc"}} className="btn btn-primary mt-3"><i className="bi bi-arrow-left"></i> Go Back</button>
         <div className="pnf">
           <img
             src={pagenotfoundImg}
