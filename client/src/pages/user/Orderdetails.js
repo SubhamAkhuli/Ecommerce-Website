287590ad410dbde9be420df97fa310ab7677b42e
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-21 12:53:46 +0530

    Update Orderdetails.js to fetch and display order details

---
+++ client/src/pages/user/Orderdetails.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+import Header from "../../components/Layout/Header";
+import Footer from "../../components/Layout/Footer";
+import UserMenu from "./UserMenu";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate, useParams } from "react-router-dom";
+
+const Orderdetails = () => {
+  const navigate = useNavigate();
+  const params = useParams();
+
+  const [order, setOrder] = useState(null);
+
+  const getOrder = async () => {
+    try {
+      const orderId = params.oid || "N/A";
+      const { data } = await axios.get(
+        `http://localhost:8080/api/v1/order/getone-order/${orderId}`
+      );
+      setOrder(data);
+    } catch (error) {
+      console.error(error);
+      toast.error("Something went wrong while fetching order.");
+    }
+  };
+
+  useEffect(() => {
+    getOrder();
+    // eslint-disable-next-line
+  }, [params.oid]);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+  return (
+    <>
+      <Header />
+      <div className="container m-3" style={{ minHeight: "65.2vh" }}>
+        <div className="row">
+          <div className="col-md-3">
+            <UserMenu />
+          </div>
+          <div className="col-md-9">
+            <div
+              className="card"
+              style={{ borderRadius: "5px", boxShadow: "0 0 10px #ccc" }}
+            >
+              <div className="card-header d-flex flex-wrap">
+              <button
+                  className="btn btn-primary "
+                  style={{borderRadius:"5px", boxShadow:"0 0 10px #ccc"}}
+                  onClick={() => {
+                    navigate(-1);
+                  }}
+                >
+                  <i class="bi bi-arrow-left"></i>
+                   Back
+                </button>
+                <h3 className="text-center mx-auto">Order Details</h3>
+              </div>
+              <div className="card-body">
+                {order ? (
+                  <>
+                    <h5>Order ID: {order._id}</h5>
+                    <p style={{marginBottom:"0px"}}><b>Order Status: </b>{order.order_status}</p>
+                    <p style={{marginBottom:"0px"}}><b>Order Date: </b>{formatDate(order.createdAt)}</p>
+                    <p style={{marginBottom:"0px"}}><b>Total Amount: ₹</b>{order.total_price}</p>
+                    <p style={{marginBottom:"0px"}}><b>Shipping Address: </b>{order.shippingAddress}</p>
+                    
+                    <p style={{marginBottom:"0px"}}>
+                      <b>Payment Time:{" "}</b>
+                      {formatDate(order.paymentResult.update_time)}
+                    </p>
+                    <h5>Order Items:</h5>
+                    <div className="row">
+                      {order.orderItems.map((item, i) => (
+                        <div key={i} className="col-md-6">
+                        <img
+                            src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${item.product}`}
+                            alt={item.name}
+                            style={{ width: "100px", height: "100px", objectFit: "contain", borderRadius: "5px",  marginBottom: "5px"}}
+                          />
+                          <p style={{marginBottom:"0px"}}><b>Seller Name: </b>{item.seller_name}</p>
+                          <p style={{marginBottom:"0px"}}><b>Product Name: </b>{item.name}</p>
+                          <p style={{marginBottom:"0px"}}><b>Quantity: </b>{item.quantity}</p>
+                          <p style={{marginBottom:"0px"}}><b>Price: ₹</b>{item.price}</p>
+                          
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                ) : (
+                  <p>Loading...</p>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Orderdetails;
