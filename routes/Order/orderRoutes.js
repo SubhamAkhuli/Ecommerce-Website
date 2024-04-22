c18b022231c58a0fe518910d72c81b81a36846f0
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-22 20:44:01 +0530

    Add new routes and controllers for seller order management

--- routes/Order/orderRoutes.js
+++ routes/Order/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../../middlewares/authMiddleware.js";
-import { getOrderByIdController, getOrdersController, paymentController, paymentTokenController } from "../../controllers/order/orderController.js";
+import { acceptOrderController, getOrderByIdController, getOrdersController, getSellerOrdersController, orderStatusUpdateController, paymentController, paymentTokenController, rejectOrderController } from "../../controllers/order/orderController.js";
 
 const router = express.Router();
 // payment gateway token
@@ -15,5 +15,16 @@ router.get("/getone-order/:id", authMiddleware, getOrderByIdController);
 // Get orders by user id
 router.get("/userorders/:pid", authMiddleware, getOrdersController); // get all orders
 
+// Get orders by seller id
+router.get("/seller-orders/:sid", authMiddleware, getSellerOrdersController); // get all orders
+
+// accept order by seller
+router.put("/accept-order/:id", authMiddleware, acceptOrderController);
+
+// reject order by seller
+router.put("/reject-order/:id", authMiddleware, rejectOrderController);
+
+// order status update by seller
+router.put("/order-status-update/:id", authMiddleware, orderStatusUpdateController);
 
 export default router;
