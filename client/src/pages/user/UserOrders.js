bb989e27f2ead2aba2461b21ef9ab18c0aff3591
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-27 17:17:35 +0530

    Refactor formatDate function in UserOrders.js to use zero-padding for day and month, and to use two-digit year format

--- client/src/pages/user/UserOrders.js
+++ client/src/pages/user/UserOrders.js
@@ -30,11 +30,12 @@ const UserOrders = () => {
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear().toString().slice(-2);
     return `${day}/${month}/${year}`;
-  };
+};
+
 
   return (
     <>
