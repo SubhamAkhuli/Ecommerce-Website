489fc0e0204d7924881020b205986bc373cbf0cd
Subham Akhuli <subhamakhuli7586@gmail.com>
2024-04-27 17:17:28 +0530

    Refactor formatDate function in Orderdetails.js to use zero-padding for day and month, and to use two-digit year format

--- client/src/pages/user/Orderdetails.js
+++ client/src/pages/user/Orderdetails.js
@@ -32,11 +32,12 @@ const Orderdetails = () => {
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear().toString().slice(-2);
     return `${day}/${month}/${year}`;
-  };
+};
+
 
   const handelcancelOrder = async (orderId) => {
     try {
